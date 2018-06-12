a31a1d1ca7f6897e08cf12e7a670626ab16e9caa
clarisse.heleno <clarisse.heleno@NOTE-CLARISSE>
2018-06-12 20:44:41 -0300

    Criando teste para 1h de estadia (falhando).

--- PrecoEstacionamento.Teste/TesteCalculoPreco.cs
+++ PrecoEstacionamento.Teste/TesteCalculoPreco.cs
@@ -19,5 +19,18 @@ namespace PrecoEstacionamento.Teste
 
             Assert.AreEqual(precoFracao, valor);
         }
+
+        [DataRow(20)]
+        [DataRow(2.5f)]
+        [DataRow(6)]
+        [DataTestMethod]
+        public void PrecoUmaHora(float precoFracao)
+        {
+            var calculo = new CalculoPreco(precoFracao);
+
+            var valor = calculo.CalculaPreco(DateTime.Parse("2018-03-10 10:00"), DateTime.Parse("2018-03-01 11:00"));
+
+            Assert.AreEqual(precoFracao * 4, valor);
+        }
     }
 }
