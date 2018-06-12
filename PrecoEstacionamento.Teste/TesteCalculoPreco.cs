a82c2f72e0593bb034383f1f3431d5ecca8dbf45
clarisse.heleno <clarisse.heleno@NOTE-CLARISSE>
2018-06-12 20:25:20 -0300

    Criando método para teste do preço da fração (falhando).

---
+++ PrecoEstacionamento.Teste/TesteCalculoPreco.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrecoEstacionamento.Web.Negocio;
+
+namespace PrecoEstacionamento.Teste
+{
+    [TestClass]
+    public class TesteCalculoPreco
+    {
+        [TestMethod]
+        public void PrecoUmaFracao()
+        {
+            var precoFracao = 2;
+            var calculo = new CalculoPreco(precoFracao);
+
+            var valor = calculo.CalculaPreco(DateTime.Parse("2018-03-10 10:00"), DateTime.Parse("2018-03-01 10:15"));
+
+            Assert.AreEqual(precoFracao, valor);
+        }
+    }
+}
---
+++ PrecoEstacionamento.Web/Negocio/CalculoPreco.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrecoEstacionamento.Web.Negocio
+{
+
+    public class CalculoPreco
+    {
+        public CalculoPreco(float precoFracao)
+        {
+        }
+
+        public object CalculaPreco(DateTime entrada, DateTime saida)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
